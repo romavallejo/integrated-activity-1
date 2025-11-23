0cbccbed1ed4744acf63e2ea8b0fd588214d8b46
a00573055 <rodrigo.martinezvall@gmail.com>
2025-11-23 16:52:34 -0600

    city done

--- Assets/Scripts/CItyCreation.cs
+++ Assets/Scripts/CItyCreation.cs
@@ -13,6 +13,7 @@ public class CItyCreation : MonoBehaviour
     x: could turn all ways
     T: untiont to street up
     L: corner
+    I: illumination
     
     0: 0
     1: 90
@@ -20,35 +21,38 @@ public class CItyCreation : MonoBehaviour
     3: 270
 
     F: filler of random tree
-    B: building
+    B: random building
+    O: fountain
+    K: bench
+    Y: fountain
     */
     List<String> cityInformation = new List<String>()
     {
-        "L1",">",">","T2","T2",">",">","T2",">","T2",">",">",">",">",">","S","T2","T2",">","T2",">","T2",">","T2","L2",
-        "^","F","F","^","v","B3","B3","^","F","P","B3","B3","B3","B3","B3","F","S","S","B3","P","F","P","B3","v","v",
-        "T1","P","B0","^","v","B2","B0","^","B2","E","E","E","B1","B1","B1","F","^","^","B1","B1","B1","B1","B1","v","v",
-        "^","B2","B0","^","v","B2","B0","^","B2","E","E","B0","L1","T2","<","<","X","X","S","<","<","<","<","T3","v",
-        "^","B2","B0","^","v","B2","B0","^","B2","E","E","P","T3","v","B3","B3","S","S","B3","B3","B3","B3","B3","v","v",
-        "T1","P","B0","^","v","B2","B0","^","B2","E","E","B0","v","v","B2","B0","^","^","B2","E","E","E","B0","v","v",
-        "^","F","F","^","v","B2","B0","^","F","B1","B1","F","v","T1","P","B0","^","T1","P","E","E","E","B0","v","v",
+        "L1",">",">","T2","T2",">",">","T2",">","T2",">",">",">",">",">","S>","T2","T2",">","T2",">","T2",">","T2","L2",
+        "^","F","F","^","v","B3","B3","^","F","P","B3","B3","B3","B3","B3","F","S^","S^","B3","P","F","P","B3","v","v",
+        "T1","P","B0","^","v","B2","B0","^","B2","E","F","K","B1","B1","B1","F","^","^","B1","B1","B1","B1","B1","v","v",
+        "^","B2","B0","^","vI","B2","B0","^","B2","Y","E","B0","L1","T2","<","<","X","X","S<","<","<","<","<","T3","v",
+        "^","B2","B0","^","v","B2","B0","^I","B2","E","E","P","T3","vI","B3","B3","S^","S^","B3","B3","B3","B3","B3","v","v",
+        "T1","P","B0","^","v","B2","B0","^","B2","F","K","B0","v","v","B2","B0","^","^","B2","Y","E","F","B0","v","v",
+        "^","F","F","^","v","B2","B0","^","F","B1","B1","F","v","T1","P","B0","^","T1","P","E","E","K","B0","v","v",
         "T1","<","<","T3","v","B2","B0","T1","T2",">","T2",">","T3","v","B2","B0","^","^","F","B1","B1","B1","B1","v","v",
-        "^","F","F","^","v","B2","B0","^","^","B2","P","B0","v","v","B2","B0","^","T1","<","<","<","<","<","T3","v",
-        "^","F","F","^","T1","P","F","^","^","B2","E","B0","v","T1","P","B0","^","L0","<","T2","<","<","<","T3","v",
-        "T1",">",">","X","S",">",">","T0","T1","B2","E","B0","v","T1","T3","F","^","F","B3","P","B3","B3","B0","v","v",
-        "T1","T2","T2","S",">","T2","T2",">","T1","B2","P","B0","S","S","^","F","^","F","B1","B1","B1","B1","B0","S","S",
-        "^","^","^","R","R","S","S",">",">",">",">","S",">",">","T0",">",">",">",">",">",">",">",">","T3","v",
-        "^","^","^","R","R","v","v","S","<","<","<","<","<","<","<","<","<","<","T2","<","<","<","<","T3","v",
-        "T1","T0","T0","T2","T2","X","X","S","<","<","<","<","L2","F","B3","B3","B3","B3","P","B3","B3","B3","F","v","v",
-        "^","F","F","S","S","v","v","F","B3","B3","B3","F","^","F","B1","B1","P","B1","B1","B1","B1","B1","F","v","v",
-        "^","B2","B0","^","^","v","v","B2","E","E","E","P","X","<","<","<","T0","<","<","T2","<","<","<","T3","v",
-        "^","B2","B0","^","^","v","v","B2","E","E","E","B0","^","F","B3","B3","P","B3","B3","P","B3","B3","F","v","v",
-        "^","B2","B0","^","^","v","T1","P","E","E","E","B0","^","B2","E","E","E","E","E","E","E","E","B0","v","v",
-        "^","F","F","^","^","v","v","B2","E","E","E","B0","^","B2","E","E","E","E","E","E","E","E","B0","v","v",
+        "^","F","F","^","v","B2","B0","^","^","B2","P","B0","v","v","B2","B0","^","T1","<","<","<","<","<","T3","vI",
+        "^I","F","K","^","T1","P","F","^","^","B2","E","B0","v","T1","P","B0","^","L0","<","T2","<","<","<","T3","v",
+        "T1",">",">","T3","ST13",">",">","T0","T1","B2","E","B0","v","T1","T3","K","^","F","B3","P","B3","B3","B0","v","v",
+        "T1","T2","T2","ST02","T0","T2","T2",">","T1","B2","P","B0","Sv","Sv","^","F","^","K","B1","B1","B1","B1","B0","Sv","Sv",
+        "^","^","^","Y","R","ST13","ST13",">",">",">I",">","S>",">",">","T0",">",">",">",">",">I",">",">","S>","T3","v",
+        "^","^","^","R","R","v","v","S<","<","<","<","<","<","<","<","<","<","<","T2","<","<","<","<","T3","v",
+        "T1","T0","T0","T2","T2","X","X","S<","<","<","<","<","L2","F","B3","B3","B3","B3","P","B3","B3","B3","F","v","v",
+        "^","F","F","S^","S^","v","v","F","B3","B3","B3","F","^","F","B1","B1","P","B1","B1","B1","B1","B1","F","v","v",
+        "^","B2","B0","^","^","v","vI","B2","E","E","K","P","X","<","<","<","T0","<","<I","T2","<","<","<","T3","v",
+        "^","B2","B0","^","^","v","v","B2","F","E","E","B0","^","F","B3","B3","P","B3","B3","P","B3","B3","F","v","v",
+        "^","B2","B0","^","^","v","T1","P","E","K","E","B0","^","B2","E","F","E","K","E","E","E","E","B0","v","v",
+        "^I","F","K","^","^","v","v","B2","F","E","F","B0","^","B2","E","E","E","E","F","E","F","E","B0","v","v",
         "^","B2","B0","^","^","v","v","F","B1","B1","B1","F","^","F","B1","B1","B1","B1","B1","B1","P","B1","F","v","v",
-        "^","B2","B0","^","T1","T0","T0",">",">",">",">",">","T0",">",">",">",">",">",">",">","T0",">",">","T3","v",
+        "^","B2","B0","^","T1","T0","T0",">I",">",">",">",">","T0",">I",">",">",">",">",">",">","T0",">",">I","T3","v",
         "^","B2","B0","^","T1",">",">",">",">",">",">",">",">",">",">",">",">",">",">",">",">",">",">","T3","v",
-        "^","F","F","^","^","F","E","F","E","F","E","F","E","F","E","F","E","F","E","F","E","F","E","v","v",
-        "L0","<","<","T0","T0","<","<","<","<","<","<","<","<","<","<","<","<","<","<","<","<","<","<","T0","L3",
+        "^","K","F","^","^","F","K","F","E","F","K","F","E","F","E","F","E","F","K","F","E","F","K","v","v",
+        "L0","<","<","T0","T0","<I","<","<","<","<","<","<","<","<","<I","<","<","<","<","<","<","<","<I","T0","L3",
     };
     public GameObject straightStreet;
     public GameObject L;
@@ -69,6 +73,12 @@ public class CItyCreation : MonoBehaviour
     public GameObject buildingG;
     public GameObject buildingH;
 
+    public GameObject bench;
+    public GameObject lightPost;
+    public GameObject fountain;
+
+    public GameObject stopLightSingle;
+    public GameObject stopLightDoble;
 
     void createCityBase()
     {
@@ -127,68 +137,73 @@ public class CItyCreation : MonoBehaviour
             }
         }
 
-        Transform child;
         for (int i = 0; i < cityPositions.Count; i++)
         {
-            if (cityInformation[i] == "<" || cityInformation[i] == ">")
+            if (cityInformation[i].Contains("<") || cityInformation[i].Contains(">"))
             {
                 GameObject horistonalStreet = Instantiate(straightStreet, cityPositions[i], Quaternion.identity);
-                child = horistonalStreet.transform.Find("default");
-                child.transform.localScale = new Vector3(0.5f,0.5f,0.5f);
-
+                horistonalStreet.transform.localScale = new Vector3(0.5f,0.5f,0.5f);
+                if (cityInformation[i].Contains("I"))
+                {
+                    GameObject light = Instantiate(lightPost,cityPositions[i],Quaternion.identity);
+                }
+                else if (cityInformation[i].Contains("S"))
+                {
+                    GameObject stopLight = Instantiate(stopLightSingle,cityPositions[i],Quaternion.identity);
+                    if (cityInformation[i].Contains(">"))
+                    {
+                        stopLight.transform.localRotation = Quaternion.Euler(0,180, 0);
+                    }
+                }
             }
-            else if (cityInformation[i] == "^" || cityInformation[i] == "v")
+            else if (cityInformation[i].Contains("^") || cityInformation[i].Contains("v"))
             {
                 GameObject verticalStreet = Instantiate(straightStreet,cityPositions[i], Quaternion.identity);
-                child = verticalStreet.transform.Find("default");
-                child.transform.localScale = new Vector3(0.5f,0.5f,0.5f);
-                child.transform.localRotation = Quaternion.Euler(0, 90, 0);
+                verticalStreet.transform.localScale = new Vector3(0.5f,0.5f,0.5f);
+                verticalStreet.transform.localRotation = Quaternion.Euler(0, 90, 0);
+
+                if (cityInformation[i].Contains("I"))
+                {
+                    GameObject light = Instantiate(lightPost,cityPositions[i],Quaternion.identity);
+                    light.transform.localRotation = Quaternion.Euler(0, 90, 0);
+                }
+                else if (cityInformation[i].Contains("S"))
+                {
+                    if (cityInformation[i].Contains("^"))
+                    {
+                        GameObject stopLight = Instantiate(stopLightSingle,cityPositions[i],Quaternion.identity);
+                        stopLight.transform.localRotation = Quaternion.Euler(0,90, 0);
+                    } 
+                    else if (cityInformation[i].Contains("v"))
+                    {
+                        GameObject stopLight = Instantiate(stopLightSingle,cityPositions[i],Quaternion.identity);
+                        stopLight.transform.localRotation = Quaternion.Euler(0,270, 0);
+                    }
+                }
             }
             else if (cityInformation[i].Contains("L"))
             {
-                GameObject verticalStreet = Instantiate(L,cityPositions[i], Quaternion.identity);
-                child = verticalStreet.transform.Find("default");
-                child.transform.localScale = new Vector3(0.5f,0.5f,0.5f);
-                switch (int.Parse(cityInformation[i][1].ToString()))
-                {
-                    case 1:
-                        child.transform.localRotation = Quaternion.Euler(0, 90, 0);
-                        break;
-                    case 2:
-                        child.transform.localRotation = Quaternion.Euler(0, 180, 0);
-                        break;
-                    case 3:
-                        child.transform.localRotation = Quaternion.Euler(0, 270, 0);
-                        break;
-                    default:
-                        break;
-                }
+                GameObject cornerStreet = Instantiate(L,cityPositions[i], Quaternion.identity);
+                cornerStreet.transform.localScale = new Vector3(0.5f,0.5f,0.5f);
+                cornerStreet.transform.localRotation = Quaternion.Euler(0,int.Parse(cityInformation[i][1].ToString())*90,0);
             }
             else if (cityInformation[i].Contains("T"))
             {
-                GameObject verticalStreet = Instantiate(T,cityPositions[i], Quaternion.identity);
-                child = verticalStreet.transform.Find("default");
-                child.transform.localScale = new Vector3(0.5f,0.5f,0.5f);
-                switch (int.Parse(cityInformation[i][1].ToString()))
+                GameObject tStreet = Instantiate(T,cityPositions[i], Quaternion.identity);
+                tStreet.transform.localScale = new Vector3(0.5f,0.5f,0.5f);
+                if (cityInformation[i].Contains("S"))
                 {
-                    case 1:
-                        child.transform.localRotation = Quaternion.Euler(0, 90, 0);
-                        break;
-                    case 2:
-                        child.transform.localRotation = Quaternion.Euler(0, 180, 0);
-                        break;
-                    case 3:
-                        child.transform.localRotation = Quaternion.Euler(0, 270, 0);
-                        break;
-                    default:
-                        break;
+                    tStreet.transform.localRotation = Quaternion.Euler(0,int.Parse(cityInformation[i][2].ToString())*90, 0);
+                    GameObject stopLight = Instantiate(stopLightSingle,cityPositions[i],Quaternion.identity);
+                    stopLight.transform.localRotation = Quaternion.Euler(0,cityInformation[i][3]*90, 0);
                 }
+                else 
+                    tStreet.transform.localRotation = Quaternion.Euler(0,int.Parse(cityInformation[i][1].ToString())*90, 0);
             }
             else if (cityInformation[i].Contains("X"))
             {
-                GameObject verticalStreet = Instantiate(X,cityPositions[i], Quaternion.identity);
-                child = verticalStreet.transform.Find("default");
-                child.transform.localScale = new Vector3(0.5f,0.5f,0.5f);
+                GameObject xStreet = Instantiate(X,cityPositions[i], Quaternion.identity);
+                xStreet.transform.localScale = new Vector3(0.5f,0.5f,0.5f);
             }
             else if (cityInformation[i] == "P")
             {
@@ -202,30 +217,25 @@ public class CItyCreation : MonoBehaviour
                 GameObject tree = Instantiate(trees[randomInt], cityPositions[i], Quaternion.identity);
                 tree.transform.localScale = new Vector3(0.5f,0.5f,0.5f);
             }
-            else if (cityInformation[i].Contains("S"))
-            {
-                //stop sign logic might go here
-            }
             else if (cityInformation[i].Contains("B"))
             {
                 int randomInt = UnityEngine.Random.Range(0, 8);
                 GameObject[] buldings = new GameObject[] {buildingA,buildingB,buildingC,buildingD,buildingE,buildingF,buildingG,buildingH};
                 GameObject bulding = Instantiate(buldings[randomInt], cityPositions[i], Quaternion.identity);
                 bulding.transform.localScale = new Vector3(0.5f,0.5f,0.5f);
-                switch (int.Parse(cityInformation[i][1].ToString()))
-                {
-                    case 1:
-                        bulding.transform.localRotation = Quaternion.Euler(0, 90, 0);
-                        break;
-                    case 2:
-                        bulding.transform.localRotation = Quaternion.Euler(0, 180, 0);
-                        break;
-                    case 3:
-                        bulding.transform.localRotation = Quaternion.Euler(0, 270, 0);
-                        break;
-                    default:
-                        break;
-                }
+                bulding.transform.localRotation = Quaternion.Euler(0,int.Parse(cityInformation[i][1].ToString())*90, 0);
+            }
+            else if (cityInformation[i] == "Y")
+            {
+                GameObject fountainArea = Instantiate(fountain,new Vector3(cityPositions[i].x+0.5f,cityPositions[i].y,cityPositions[i].z+0.5f),Quaternion.identity);
+                int randomInt = UnityEngine.Random.Range(0, 4);
+                fountainArea.transform.localRotation = Quaternion.Euler(0, randomInt*90, 0);
+            }
+            else if (cityInformation[i] == "K")
+            {
+                GameObject benchArea = Instantiate(bench,cityPositions[i],Quaternion.identity);
+                int randomInt = UnityEngine.Random.Range(0, 4);
+                benchArea.transform.localRotation = Quaternion.Euler(0, randomInt*90, 0);
             }
         }
     }
